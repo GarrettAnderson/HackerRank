7e07505b6525f61558e5808aa9d49da9d0596a2d
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2021-02-23 23:27:46 -0500

    added more code working through the challenge

--- LessonChallenges/Day5-TemplateLiterals.js
+++ LessonChallenges/Day5-TemplateLiterals.js
@@ -1,5 +1,15 @@
 function sides(literals, ...expressions) {
+
     let str = literals
     let input = expressions
-    console.log(input)
+    console.log(input[0], input[1])
+
+    const area = input[0]
+    const perim = input[1]
+
+    let s1 = (perim + Math.sqrt((Math.pow(perim, 2) - 16) * area))/4
+    let s2 = (perim - Math.sqrt((Math.pow(perim, 2) - 16) * area))/4
+
+    console.log(s1, s2)
+
 }
