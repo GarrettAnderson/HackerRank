4a0a644ecf23b22c0a8d4fa3d38f1c2c6bd41e4c
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2021-02-24 18:07:12 -0500

    added more code working through the challenge. created array to store measurement values in order to sort them and print values in ascending order. Got the values o be saved in ascending order in an array. passed the inital test case

--- LessonChallenges/Day5-TemplateLiterals.js
+++ LessonChallenges/Day5-TemplateLiterals.js
@@ -1,31 +1,32 @@
 function sides(literals, ...expressions) {
-    let str = literals
-    let input = expressions
-    // console.log(input[0], input[1])
-    let measurements = []
-    let sorted = []
+  let str = literals
+  let input = expressions
+  // console.log(input[0], input[1])
+  let measurements = []
+  let sorted = []
 
 
-    const area = input[0]
-    const perim = input[1]
+  const area = input[0]
+  const perim = input[1]
 
-    let s1 = (perim + (Math.sqrt(Math.pow(perim, 2) - (16 * area))))/4
-    let s2 = (perim - (Math.sqrt(Math.pow(perim, 2) - (16 * area))))/4
+  let s1 = (perim + (Math.sqrt(Math.pow(perim, 2) - (16 * area)))) / 4
+  let s2 = (perim - (Math.sqrt(Math.pow(perim, 2) - (16 * area)))) / 4
 
-    // console.log(s1, s2)
-    measurements.push(s1)
-    measurements.push(s2)
-    // console.log(measurements)
+  // console.log(s1, s2)
+  measurements.push(s1)
+  measurements.push(s2)
+  // console.log(measurements)
 
-    measurements.map((val, acc) => {
-        // sorted.push(measurements[acc])
-        if (measurements[acc + 1] < measurements[acc]) {
-            sorted.push(measurements[acc + 1])
-            console.log(sorted[acc])
-        } else {
-            sorted.push(measurements[acc - 1])
-            console.log(sorted[acc])
-        }
-            // console.log(sorted)
-    })
+  measurements.map((val, acc) => {
+    // sorted.push(measurements[acc])
+    if (measurements[acc + 1] < measurements[acc]) {
+      sorted.push(measurements[acc + 1])
+      // console.log(sorted[acc])
+    } else {
+      sorted.push(measurements[acc - 1])
+      // console.log(sorted[acc])
+    }
+    // console.log(sorted)
+  })
+  return sorted
 }
