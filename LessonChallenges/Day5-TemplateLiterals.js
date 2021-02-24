9bb98f8f2868c7cbe4748a2dd5ccf799f3d53bea
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2021-02-24 16:39:53 -0500

    added more code working through the challenge. created array to store measurement values in order to sort them and print values in ascending order.

--- LessonChallenges/Day5-TemplateLiterals.js
+++ LessonChallenges/Day5-TemplateLiterals.js
@@ -3,6 +3,8 @@ function sides(literals, ...expressions) {
     let input = expressions
     // console.log(input[0], input[1])
     let measurements = []
+    let sorted = []
+
 
     const area = input[0]
     const perim = input[1]
@@ -13,5 +15,18 @@ function sides(literals, ...expressions) {
     // console.log(s1, s2)
     measurements.push(s1)
     measurements.push(s2)
-    console.log(measurements)
+    // console.log(measurements)
+
+    measurements.map((val, acc) => {
+        sorted.push(measurements[acc])
+        if (sorted[acc] < sorted[acc + 1]) {
+            sorted.push(measurements[acc])
+            // console.log(sorted)
+        } else {
+            // sorted.push(measurements[acc])
+            console.log(sorted)
+
+        }
+            // console.log(sorted)
+    })
 }
